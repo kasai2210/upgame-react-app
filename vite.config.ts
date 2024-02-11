d862df2515372da4367423432180880eefbece5b
Bhoopen Sawant <bsawant@microsoft.com>
2024-02-11 21:30:41 +0530

    chanding vit config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for bundled files
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js', // Use chunk name for JS files
+      },
+    },
+  },
 })
